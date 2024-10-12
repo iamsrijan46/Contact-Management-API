2a5f421e081362eaad64acff7520a87e61e85579
Srijan Mani Tripathi <srijanmanitripathi2002@gmail.com>
2024-10-12 14:52:03 +0530

    add model for user

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const connectDb = require("./config/db");
 connectDb();
 
 const contact = require("./routes/contactRoutes");
+const user = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT;
 
 app.use('/api/contacts', contact);
+app.use('/api/user', user);
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);    
---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = reuire('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {timestamp: true});
+
+module.exports = mongoose.model('User', userSchema);
